5f0d79df84c056b420e98b85f92fa9f7110c0284
José Winny <jose.winny.dev@gmail.com>
2023-06-26 15:01:10 -0300

    Add first icon/button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,6 +8,21 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+      //First Button  
+        leading: Container(
+          alignment: Alignment.center,
+          margin: const EdgeInsets.all(10),          decoration: BoxDecoration(
+            color: Colors.transparent,
+            borderRadius: BorderRadius.circular(10)
+          ),
+        //Icon
+          child: SvgPicture.asset("assets/icons/Arrow - Left 2.svg",
+           width: 20, 
+           height: 20
+           )
+        ),
+
+      //Title
         title: const Text("Breakfast",
         style: TextStyle(
           color: Colors.black,
@@ -16,6 +32,8 @@ class HomePage extends StatelessWidget {
         elevation: 0.0,
         centerTitle: true,
         backgroundColor: Colors.transparent,
+      
+      
       ),
     );
   }
